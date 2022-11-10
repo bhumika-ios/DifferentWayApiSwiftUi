bcad0cb051e2b99ffa344f469ba500ebf1a1de18
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-10 16:23:26 +0530

    Initial Commit

---
+++ DifferentWayApiSwiftUi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DifferentWayApiSwiftUi
+//
+//  Created by Bhumika Patel on 10/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DifferentWayApiSwiftUi/DifferentWayApiSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DifferentWayApiSwiftUiApp.swift
+//  DifferentWayApiSwiftUi
+//
+//  Created by Bhumika Patel on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DifferentWayApiSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
